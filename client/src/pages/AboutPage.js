52511ca6400d7a7f229894274491ef6fe8b1cca2
WyattAlexander-001 <wyattbushman117@gmail.com>
2024-04-22 21:37:02 -0400

    finished about page

--- client/src/pages/AboutPage.js
+++ client/src/pages/AboutPage.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
-//import '../generalCSS.css'; // Link to the CSS file
-//import './AboutPage.css'; // Link to the CSS file
+import './AboutPage.css'; // Import the CSS here
+import avocadoImage from '../images/zutanoAvocado.jpeg';
+
 
 const AboutPage = () => {
   return (
     <>
-    <Navbar />
-    <div className="about-container" style={{ paddingTop: '60px' }}>
-      <img src="path_to_image.jpg" alt="About Us" className="about-image" />
-      <div className="about-text">
-        <h1>Mission Statement</h1>
-        <p>Our mission is to spread the love and knowledge of avocados to the world.</p>
-        <h2>Who We Are</h2>
-        <p>Bio about the organization or individual.</p>
+      <Navbar />
+      <div className="about-container">
+        <img src={avocadoImage} alt="About Us" className="about-image" />
+        <div className="about-text">
+          <h1>Mission Statement</h1>
+          <p>Our mission is to spread the love and knowledge of avocados to the world.</p>
+          <h2>Who We Are</h2>
+          <p>Bio about the organization or individual.</p>
+        </div>
       </div>
-    </div>
     </>
   );
 };
--- client/src/pages/ContactPage.js
+++ client/src/pages/ContactPage.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Navbar from '../components/Navbar';
 //import '../generalCSS.css'; // Link to the CSS file
-//import './ContactPage.css'; // Link to the CSS file
+import './ContactPage.css';
 
 const ContactPage = () => {
   const [email, setEmail] = useState('');
@@ -14,20 +14,18 @@ const ContactPage = () => {
 
   return (
     <>
-    <Navbar />
-    <div style={{ paddingTop: '60px' }}>
-      <div className="contact-form">
-        <h1>Contact Us</h1>
-        <p>Customer Service: (+1)123-456-7890</p>
-        <form onSubmit={handleSubmit}>
-          <input type="email" placeholder="Your Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-          <br></br>
-          <textarea placeholder="Your Message" value={message} onChange={(e) => setMessage(e.target.value)} required />
-          <br></br>
-          <button type="submit">Send Inquiry</button>
-        </form>
+      <Navbar />
+      <div className="contact-container">
+        <div className="contact-form">
+          <h1>Contact Us</h1>
+          <p>Customer Service: (+1)123-456-7890</p>
+          <form onSubmit={handleSubmit}>
+            <input type="email" placeholder="Your Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+            <textarea placeholder="Your Message" value={message} onChange={(e) => setMessage(e.target.value)} required />
+            <button type="submit">Send Inquiry</button>
+          </form>
+        </div>
       </div>
-    </div>
     </>
   );
 };
