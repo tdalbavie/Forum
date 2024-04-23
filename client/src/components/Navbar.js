b3e6dd561962172b5d865785dd2454d87372f41e
tdalbavie <thomasdalbavie@gmail.com>
2024-04-23 14:46:41 -0400

    Fixed Experience

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -37,21 +37,21 @@ function Navbar() {
       </button>
       <span className="navbar-title">All About Avocados</span>
       <ul className={isOpen ? 'show-dropdown' : ''}>
-        <li><Link to="/" onClick={toggleDropdown}>Home</Link></li>
-        <li><Link to="/Signup" onClick={toggleDropdown}>Sign Up</Link></li>
-        <li><Link to="/Login" onClick={toggleDropdown}>Login</Link></li>
-        <li><Link to="/Categories" onClick={toggleDropdown}>Forum</Link></li>
-        <li><Link to="/Experience-Check" onClick={toggleDropdown}>Experience Check</Link></li>
-        <li><Link to="/Recipes" onClick={toggleDropdown}>Recipes</Link></li>
-        <li><Link to="/CreateRecipe" onClick={toggleDropdown}>Create Recipe</Link></li>
-        <li><Link to="/articles" onClick={toggleDropdown}>Articles</Link></li>
-        <li><Link to="/create-article" onClick={toggleDropdown}>Create Article</Link></li>
-        <li><Link to="/avocado-101" onClick={toggleDropdown}>Avocado 101</Link></li>
-        <li><Link to="/productForm" onClick={toggleDropdown}>Insert Product</Link></li>
-        <li><Link to="/checkout" onClick={toggleDropdown}>Checkout</Link></li>
-        <li><Link to="/pay" onClick={toggleDropdown}>Payment Method</Link></li>
-        <li><Link to="/about" onClick={toggleDropdown}>About</Link></li>
-        <li><Link to="/contact" onClick={toggleDropdown}>Contact</Link></li>
+        <li><Link to="/" onClick={() => { toggleDropdown(); handleExperiencePoints(); }}>Home</Link></li>
+        <li><Link to="/Signup" onClick={() => { toggleDropdown(); handleExperiencePoints(); }}>Sign Up</Link></li>
+        <li><Link to="/Login" onClick={() => { toggleDropdown(); handleExperiencePoints(); }}>Login</Link></li>
+        <li><Link to="/Categories" onClick={() => { toggleDropdown(); handleExperiencePoints(); }}>Forum</Link></li>
+        <li><Link to="/Experience-Check" onClick={() => { toggleDropdown(); handleExperiencePoints(); }}>Experience Check</Link></li>
+        <li><Link to="/Recipes" onClick={() => { toggleDropdown(); handleExperiencePoints(); }}>Recipes</Link></li>
+        <li><Link to="/CreateRecipe" onClick={() => { toggleDropdown(); handleExperiencePoints(); }}>Create Recipe</Link></li>
+        <li><Link to="/articles" onClick={() => { toggleDropdown(); handleExperiencePoints(); }}>Articles</Link></li>
+        <li><Link to="/create-article" onClick={() => { toggleDropdown(); handleExperiencePoints(); }}>Create Article</Link></li>
+        <li><Link to="/avocado-101" onClick={() => { toggleDropdown(); handleExperiencePoints(); }}>Avocado 101</Link></li>
+        <li><Link to="/productForm" onClick={() => { toggleDropdown(); handleExperiencePoints(); }}>Insert Product</Link></li>
+        <li><Link to="/checkout" onClick={() => { toggleDropdown(); handleExperiencePoints(); }}>Checkout</Link></li>
+        <li><Link to="/pay" onClick={() => { toggleDropdown(); handleExperiencePoints(); }}>Payment Method</Link></li>
+        <li><Link to="/about" onClick={() => { toggleDropdown(); handleExperiencePoints(); }}>About</Link></li>
+        <li><Link to="/contact" onClick={() => { toggleDropdown(); handleExperiencePoints(); }}>Contact</Link></li>
       </ul>
     </nav>
   );
