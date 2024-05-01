74aad5f7b47b3eff77d4ab21071fd4fdc9c45ecd
tdalbavie <thomasdalbavie@gmail.com>
2024-05-01 12:36:28 -0400

    Updated Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import "./Navbar.css"; // Make sure this is correctly pointing to your CSS file
-import { useUser } from "../context/UserContext";
-import axios from "axios";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css'; // Make sure this is correctly pointing to your CSS file
+import { useUser } from '../context/UserContext';
+import axios from 'axios';
 import shoppingCartIcon from "../images/shoppingCartIcon.jpg"; // import the image file
 
 function Navbar() {
@@ -15,19 +15,16 @@ function Navbar() {
   const handleExperiencePoints = async () => {
     if (user) {
       try {
-        const response = await axios.post(
-          "http://localhost:9000/users/add-experience",
-          {
-            userId: user._id,
-            pointsToAdd: 10,
-          }
-        );
+        const response = await axios.post('http://localhost:9000/users/add-experience', {
+          userId: user._id,
+          pointsToAdd: 10,
+        });
 
         if (response.status === 200) {
-          console.log("Experience points added successfully");
+          console.log('Experience points added successfully');
         }
       } catch (error) {
-        console.error("Error adding experience points:", error);
+        console.error('Error adding experience points:', error);
       }
     }
   };
@@ -49,77 +46,22 @@ function Navbar() {
           />
         </Link>
       </div>
-      <ul className={isOpen ? "show-dropdown" : ""}>
-        <li>
-          <Link to="/" onClick={toggleDropdown}>
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/Signup" onClick={toggleDropdown}>
-            Sign Up
-          </Link>
-        </li>
-        <li>
-          <Link to="/Login" onClick={toggleDropdown}>
-            Login
-          </Link>
-        </li>
-        <li>
-          <Link to="/Categories" onClick={toggleDropdown}>
-            Forum
-          </Link>
-        </li>
-        <li>
-          <Link to="/Recipes" onClick={toggleDropdown}>
-            Recipes
-          </Link>
-        </li>
-        <li>
-          <Link to="/CreateRecipe" onClick={toggleDropdown}>
-            Create Recipe
-          </Link>
-        </li>
-        <li>
-          <Link to="/articles" onClick={toggleDropdown}>
-            Articles
-          </Link>
-        </li>
-        <li>
-          <Link to="/create-article" onClick={toggleDropdown}>
-            Create Article
-          </Link>
-        </li>
-        <li>
-          <Link to="/avocado-101" onClick={toggleDropdown}>
-            Avocado 101
-          </Link>
-        </li>
-        <li>
-          <Link to="/productForm" onClick={toggleDropdown}>
-            Insert Product
-          </Link>
-        </li>
-        <li>
-          <Link to="/checkout" onClick={toggleDropdown}>
-            Checkout
-          </Link>
-        </li>
-        <li>
-          <Link to="/pay" onClick={toggleDropdown}>
-            Payment Method
-          </Link>
-        </li>
-        <li>
-          <Link to="/about" onClick={toggleDropdown}>
-            About
-          </Link>
-        </li>
-        <li>
-          <Link to="/contact" onClick={toggleDropdown}>
-            Contact
-          </Link>
-        </li>
+      <ul className={isOpen ? 'show-dropdown' : ''}>
+        <li><Link to="/" onClick={() => { toggleDropdown(); handleExperiencePoints(); }}>Home</Link></li>
+        <li><Link to="/Signup" onClick={() => { toggleDropdown(); handleExperiencePoints(); }}>Sign Up</Link></li>
+        <li><Link to="/Login" onClick={() => { toggleDropdown(); handleExperiencePoints(); }}>Login</Link></li>
+        <li><Link to="/Categories" onClick={() => { toggleDropdown(); handleExperiencePoints(); }}>Forum</Link></li>
+        <li><Link to="/Experience-Check" onClick={() => { toggleDropdown(); handleExperiencePoints(); }}>Experience Check</Link></li>
+        <li><Link to="/Recipes" onClick={() => { toggleDropdown(); handleExperiencePoints(); }}>Recipes</Link></li>
+        <li><Link to="/CreateRecipe" onClick={() => { toggleDropdown(); handleExperiencePoints(); }}>Create Recipe</Link></li>
+        <li><Link to="/articles" onClick={() => { toggleDropdown(); handleExperiencePoints(); }}>Articles</Link></li>
+        <li><Link to="/create-article" onClick={() => { toggleDropdown(); handleExperiencePoints(); }}>Create Article</Link></li>
+        <li><Link to="/avocado-101" onClick={() => { toggleDropdown(); handleExperiencePoints(); }}>Avocado 101</Link></li>
+        <li><Link to="/productForm" onClick={() => { toggleDropdown(); handleExperiencePoints(); }}>Insert Product</Link></li>
+        <li><Link to="/checkout" onClick={() => { toggleDropdown(); handleExperiencePoints(); }}>Checkout</Link></li>
+        <li><Link to="/pay" onClick={() => { toggleDropdown(); handleExperiencePoints(); }}>Payment Method</Link></li>
+        <li><Link to="/about" onClick={() => { toggleDropdown(); handleExperiencePoints(); }}>About</Link></li>
+        <li><Link to="/contact" onClick={() => { toggleDropdown(); handleExperiencePoints(); }}>Contact</Link></li>
       </ul>
     </nav>
   );
